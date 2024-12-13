a03ea5e248bb836c45853cc80d7b11893a47eb73
Pedro Henrique <pedro.henrique.martins404@gmail.com>
2024-12-12 22:22:51 -0300

    feat: :seedling: Start Project

---
+++ test/svc-coffee-api.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SvcCoffeeApi from '../lib/svc-coffee-api-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/svc-coffee-api-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SvcCoffeeApi.SvcCoffeeApiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
